019ee2d6f72a4baee6a29d08d611434401a06094
Victor-Hugo0147 <118931151+Victor-Hugo0147@users.noreply.github.com>
2023-06-12 22:57:11 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -1,67 +1,110 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = 3000;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+app.use((request, response, next) =>{
+  response.header("Access-Control-Allow-Origin", "*");
+  response.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PATCH,DELETE");
+  response.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+  next();
+})
+// Rota para servir o arquivo HTML estático
+app.use(express.static('public'));
 
-let users = []; // Array para armazenar os usuários (simulação de um banco de dados)
-
-// Rota para adicionar um usuário
-app.post('/users', (req, res) => {
-    console.log('kkkk')
-    const { name, email } = req.body;
-    const newUser = {
-      name,
-      email
-    };
-    users.push(newUser);
-    res.json({ message: 'Usuário adicionado com sucesso!' });
+// Conexão com o MongoDB
+mongoose.connect('mongodb+srv://leo9aec:unifor239@cluster0.q6au30c.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
+  .then(() => {
+    console.log('Conexão com o MongoDB estabelecida com sucesso!');
+  })
+  .catch((error) => {
+    console.error('Erro ao conectar com o MongoDB:', error);
   });
 
+// Definição do modelo do usuário
+const User = mongoose.model('User', {
+  name: String,
+  email: String
+});
+
+// Rota para adicionar um usuário
+app.post('/user', (req, res) => {
+  const { name, email } = req.body;
+
+  // Cria uma nova instância do modelo User com os dados recebidos
+  const newUser = new User({
+    name,
+    email
+  });
+
+  // Salva o usuário no MongoDB
+  newUser.save()
+    .then(() => {
+      res.json({ message: 'Usuário adicionado com sucesso!' });
+    })
+    .catch((error) => {
+      console.error('Erro ao adicionar usuário:', error);
+      res.status(500).json({ message: 'Erro ao adicionar usuário.' });
+    });
+});
 
 // Rota para exibir todos os usuários
 app.get('/users', (req, res) => {
-  res.json(users);
+  // Busca todos os usuários no MongoDB
+  User.find()
+    .then((users) => {
+      res.json(users);
+    })
+    .catch((error) => {
+      console.error('Erro ao buscar usuários:', error);
+      res.status(500).json({ message: 'Erro ao buscar usuários.' });
+    });
 });
 
 // Rota para editar um usuário
-app.put('/users/:email', (req, res)=> {
-    console.log('Rota de edição funcionando');
-    const { email } = req.params;
-    const { nome, senha } = req.body;
-  
-    // Verifica se o usuário existe
-    const userIndex = users.findIndex(user => user.email === email);
-    if (userIndex === -1) {
-      return res.status(404).json({ message: 'Usuário não encontrado.' });
-    }
-  
-    // Atualiza os atributos do usuário
-    users[userIndex].nome = nome;
-    users[userIndex].senha = senha;
-  
-    res.json({ message: 'Usuário atualizado com sucesso.', user: users[userIndex] });
-  });
+app.put('/users/:email', (req, res) => {
+  const { email } = req.params;
+  const { name, password } = req.body;
+
+  // Busca e atualiza o usuário no MongoDB
+  User.findOneAndUpdate({ email }, { name, password }, { new: true })
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: 'Usuário não encontrado.' });
+      }
+      res.json({ message: 'Usuário atualizado com sucesso.', user });
+    })
+    .catch((error) => {
+      console.error('Erro ao atualizar usuário:', error);
+      res.status(500).json({ message: 'Erro ao atualizar usuário.' });
+    });
+});
 
 // Rota para excluir um usuário
 app.delete('/users/:email', (req, res) => {
-    const { email } = req.params;
-  
-    // Verifica se o usuário existe
-    const userIndex = users.findIndex(user => user.email === email);
-    if (userIndex === -1) {
-      return res.status(404).json({ message: 'Usuário não encontrado.' });
-    }
-  
-    // Remove o usuário do array
-    const deletedUser = users.splice(userIndex, 1)[0];
-  
-    res.json({ message: 'Usuário excluído com sucesso.', user: deletedUser });
-  });
-  
+  const { email } = req.params;
+
+  // Remove o usuário do MongoDB
+  User.findOneAndDelete({ email })
+    .then((deletedUser) => {
+      if (!deletedUser) {
+        return res.status(404).json({ message: 'Usuário não encontrado.' });
+      }
+      res.json({ message: 'Usuário excluído com sucesso.', user: deletedUser });
+    })
+    .catch((error) => {
+      console.error('Erro ao excluir usuário:', error);
+      res.status(500).json({ message: 'Erro ao excluir usuário.' });
+    });
+});
+
 // Inicializa o servidor
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
